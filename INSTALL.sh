55044143b1bc28fafe155c4811c68a435b8f1cdd
Marcos Cruz <github@programandala.net>
2017-09-11 20:49:34 +0200

    Improve installer and uninstaller

--- INSTALL.sh
+++ INSTALL.sh
@@ -5,44 +5,45 @@
 # This file is part of SBim
 # http://programandala.net/es.programa.sbim.html
 
-# ##############################################################
+# ==============================================================
+# Description
+
+# SBim installer.
+#
+# Usage:
+#
+#   ./INSTALL.sh
+
+# ==============================================================
 # Author and license
 
-# Author: Marcos Cruz (programandala.net), 2015, 2016
+# Author: Marcos Cruz (programandala.net), 2015, 2016, 2017
 
 # You may do whatever you want with this work, so long as you
 # retain the copyright/authorship/acknowledgment/credit
 # notice(s) and this license in all redistributed copies and
 # derived works.  There is no warranty.
 
-# ##############################################################
-# Description
+# ==============================================================
 
-# This program installs SBim.
-#
-# Edit <CONFIG.sh> first to suit your system.
+install -D --m 644 sbim.converter.vim ~/.vim/sbim.vim
+install -D --m 644 sbim.ftdetect.vim ~/.vim/ftdetect/sbim.vim
+install -D --m 644 sbim.ftplugin.vim ~/.vim/ftplugin/sbim.vim
+install -D --m 644 sbim.syntax.vim ~/.vim/syntax/sbim.vim
+install -D --m 644 superbasic.syntax.vim ~/.vim/syntax/superbasic.vim
 
-# ##############################################################
-# Usage
+exit 0
 
-#   INSTALL.sh
-
-# ##############################################################
-# History
+# ==============================================================
+# Change log
 
 # 2015-12-26: First version.
+#
 # 2016-01-19: Update header.
+#
 # 2016-01-25: Add <sbim.ftplugin.vim>.
+#
 # 2016-12-09: Add syntax files.
-
-# ##############################################################
-
-. ./CONFIG.sh
-
-eval ${INSTALLCMD}sbim.converter.vim $VIMDIR/sbim.vim
-eval ${INSTALLCMD}sbim.ftdetect.vim $VIMDIR/ftdetect/sbim.vim
-eval ${INSTALLCMD}sbim.ftplugin.vim $VIMDIR/ftplugin/sbim.vim
-eval ${INSTALLCMD}sbim.syntax.vim $VIMDIR/syntax/sbim.vim
-eval ${INSTALLCMD}superbasic.syntax.vim $VIMDIR/ftplugin/superbasic.vim
-
-# vim: textwidth=64
+#
+# 2017-09-11: Fix installation of SuperBASIC syntax file.  Rewrite
+# with `install`. Remove <CONFIG.sh>. Use <~/.vim>.
--- UNINSTALL.sh
+++ UNINSTALL.sh
@@ -5,43 +5,44 @@
 # This file is part of SBim
 # http://programandala.net/es.programa.sbim.html
 
-# ##############################################################
+# ==============================================================
+# Description
+
+# SBim uninstaller.
+#
+# Usage:
+#
+#   ./UNINSTALL.sh
+
+# ==============================================================
 # Author and license
 
-# Author: Marcos Cruz (programandala.net), 2015, 2016
+# Author: Marcos Cruz (programandala.net), 2015, 2016, 2017
 
 # You may do whatever you want with this work, so long as you
 # retain the copyright/authorship/acknowledgment/credit
 # notice(s) and this license in all redistributed copies and
 # derived works.  There is no warranty.
 
-# ##############################################################
-# Description
+# ==============================================================
 
-# This program uninstalls SBim.
-#
-# Edit <CONFIG.sh> first to suit your system.
+rm -f ~/.vim/sbim.vim
+rm -f ~/.vim/ftdetect/sbim.vim
+rm -f ~/.vim/ftplugin/sbim.vim
+rm -f ~/.vim/syntax/sbim.vim
+rm -f ~/.vim/syntax/superbasic.vim
 
-# ##############################################################
-# Usage
+exit 0
 
-#   UNINSTALL.sh
-
-# ##############################################################
-# History
+# ==============================================================
+# Change log
 
 # 2015-12-26: First version.
+#
 # 2016-01-19: Update header.
+#
 # 2016-01-25: Add <ftplugin/sbim.vim>.
+#
 # 2016-12-09: Add syntax files.
-
-# ##############################################################
-
-. ./CONFIG.sh
-
-rm -f $VIMDIR/sbim.vim
-rm -f $VIMDIR/ftdetect/sbim.vim
-rm -f $VIMDIR/ftplugin/sbim.vim
-rm -f $VIMDIR/syntax/sbim.vim
-rm -f $VIMDIR/syntax/superbasic.vim
-
+#
+# 2017-09-11: Remove <CONFIG.sh>. Use <~/.vim>.
